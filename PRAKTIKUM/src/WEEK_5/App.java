4e0f6320a39bc7fe20711451c1224953300d96ba
IamJayenn <sangadpintar@gmail.com>
2025-03-25 20:04:42 +0700

    delete files

--- PRAKTIKUM/src/WEEK_5/App.java
+++
@@ -1,8 +0,0 @@
-package WEEK_5;
-public class App {
-  public static void main(String[] args) throws Exception {
-    LandVehicle car = new LandVehicle("Car", 100, 100, 4);
-    car.move();
-    car.calculateFuelConsumption(50);
-  }
-}
