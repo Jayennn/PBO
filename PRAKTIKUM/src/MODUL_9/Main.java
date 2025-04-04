4508e3841ac5dec39c19cab7d339deb14343ef38
IamJayenn <sangadpintar@gmail.com>
2025-04-04 13:12:17 +0700

    Modul 9: Done

---
+++ PRAKTIKUM/src/MODUL_9/Main.java
@@ -0,0 +1,13 @@
+package MODUL_9;
+
+public class Main {
+
+  public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        new Calculator();
+      }
+
+    });
+  }
+}
